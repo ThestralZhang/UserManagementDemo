fbc13fb6f58cb67a699c0631de95984e89038bba
Keith Zhang <thestralzhang@gmail.com>
2019-04-03 13:13:37 +0800

    webpack configured

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// import './index.less';
+import './test.css';
+
+console.log('success');
---
+++ webpack.base.conf.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
