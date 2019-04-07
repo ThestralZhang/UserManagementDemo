d2a41901d2a0b11064be1c16a4e230628e90aacb
张挺然 <thestralzhang@gmail.com>
2019-04-07 18:37:25 +0800

    HMR configured

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Router, Route, hashHistory } from "react-router-3";
-// import store from "store";
 import "./test.css";
 import "./index.less";
-import {editAccount, deleteAccount, addAccount} from "./actions/index";
 
 
-ReactDOM.render(
+const render = _ => ReactDOM.render(
     <App />,
-  document.getElementById("root")
+    document.getElementById("root")
 );
 
-//
-// console.log(store.getState());
-//
-// const unsubscribe = store.subscribe(_ => console.log(store.getState()));
-//
-// store.dispatch(
-//     addAccount({ nickname: "ladkl", realname: "Slder", email: "dewf@fw.vre" })
-// );
-// store.dispatch(
-//     addAccount({ nickname: "jll", realname: "Lsif", email: "reaf@fw.vre" })
-// );
-// store.dispatch(
-//     editAccount(0, {
-//         nickname: "ofewww",
-//         realname: "Slder",
-//         email: "dewf@fw.vre"
-//     })
-// );
-// store.dispatch(deleteAccount(1));
+render();
 
+if(module.hot){
+    module.hot.accept("./App", _ => render());
+}
